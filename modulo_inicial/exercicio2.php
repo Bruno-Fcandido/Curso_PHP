ef10ae1f823a9873bc5612fa87789c007e466fc7
Bruno-Fcandido <brunofercandido@gmail.com>
2022-06-21 21:53:48 -0300

    exercício 2

---
+++ modulo_inicial/exercicio2.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Informe um numero de 1 a 9";
+$n = readline();
+
+
+if($n<1){
+
+    echo"nao esta na faixa";
+}
+
+elseif($n>9){
+
+    echo"nao esta na faixa";
+}
+
+else{
+    echo "esta na faixa";
+}
+
+?>
