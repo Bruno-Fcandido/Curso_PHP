883edf9d9678c2f35a1a6cb97605a5d48d58ed7f
Bruno-Fcandido <brunofercandido@gmail.com>
2022-06-21 21:45:32 -0300

    sintaxe
    sintaxe basica do PHP

---
+++ modulo_inicial/base_sintaxe.php
@@ -0,0 +1,49 @@
+<?php
+
+echo'parada pro cafe';
+
+$nomevariavel = 22;
+
+define('NOME_CONSTANTE',10);
+
+//Comentario de uma linha
+#comentario de uma linha
+
+/*
+varias linhas
+de comentario*/
+
+
+//operadores aritmeticos    + - / %
+
+$soma = ($n1 + $n2);
+
+$subtracao = ($n1 - $n2);
+
+$multiplicacao = ($n1 * $n2);
+
+$divisao = ($n1 / $n2);
+
+# operadores Relacionais   > < == !=
+
+
+$n3 = ($n1 > $n2);
+
+$n4 = ($n1 < $n2);
+
+$n5 = ($n1 == $n2);
+
+$n6 = ($n1 != $n2);
+
+#operadores logicos    AND OR NOT
+
+$logicaand = (($n1 + $n2) and ($n1==$n2) );
+
+$logicaor = (($n1 + $n2) or ($n1==$n2) );
+
+$logicaNOT = (!($n1==$n2));
+
+//TERMINAL
+$variavel = readline();
+
+ ?>
