c6dca1488e4305a9bd17e7ed236a63ae2abd9140
Bruno-Fcandido <brunofercandido@gmail.com>
2022-06-21 22:17:26 -0300

    Exercicio3

---
+++ modulo_inicial/exercicio3.php
@@ -0,0 +1,27 @@
+<?php
+
+$feminino;
+$masculino;
+
+echo"Informe seu nome";
+$nome = readline();
+
+
+echo"informe seu sexo Masculino ou Feminino";
+$genero=readline();
+
+
+if($genero=="Feminino"){
+echo"Ilma Sra.";
+echo $nome;  
+}else{
+    $genero=="Masculino";
+    echo"Ilmo Sr.";
+    echo $nome;
+}
+
+ 
+       
+
+
+?>
